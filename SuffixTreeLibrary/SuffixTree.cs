20705dd7ff0d89e170c4e75129f7538705b40caa
MYriad1993 <myriad2006@ya.ru>
2019-02-27 18:43:21 +0500

    Store indexes and lengths instead of substrings

--- SuffixTreeLibrary/SuffixTree.cs
+++ SuffixTreeLibrary/SuffixTree.cs
@@ -1,26 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SuffixTreeLibary
 {
     public class SuffixTree
     {
-        private readonly string text;
-
-        private readonly Node rootNode = new Node(string.Empty);
+        private readonly string originalText;
+        private readonly Node rootNode;
 
         public SuffixTree(string text)
         {
-            this.text = text + "$";
+            originalText = text + "$";
+            rootNode = new Node(originalText.Length, 0, originalText);
 
-			BuildNLogN();
+            BuildNLogN();
         }
 
         public bool Contains(string substring)
         {
             bool Contains(string s, Node node)
             {
-                if (!node.FindChild(s[0], out var child))
+                if (!node.TryGetChild(s[0], out var child))
                     return false;
 
                 if (s.Length == 1)
@@ -34,71 +35,80 @@ namespace SuffixTreeLibary
 
         private void BuildNLogN()
         {
-            void AddChildToNode(string addingSuffix, Node node)
+            void AddChildToNode(int startIndex, Node node)
 			{
-				if (node.FindChild(addingSuffix[0], out var child))
+                var addingSuffix = originalText.Substring(startIndex);
+				if (node.TryGetChild(originalText[startIndex], out var child))
 				{
-				    int difIndex;
-				    for (difIndex = 1; difIndex < child.Text.Length; difIndex++)
-						if (addingSuffix[difIndex] != child.Text[difIndex])
-							break;
-
-				    var addingSuffixRest = addingSuffix.Substring(difIndex);
-					if (difIndex == child.Text.Length)
-					    AddChildToNode(addingSuffixRest, child);
+				    int difIndex = child.Compare(startIndex);
+					if (difIndex == child.Length)
+					    AddChildToNode(startIndex + child.Length, child);
 					else
 					{
 					    child.Split(difIndex);
-						child.AddChild(addingSuffixRest);
+						child.AddChild(startIndex + difIndex);
 					}
 				}
 				else
-				    node.AddChild(addingSuffix);
+				    node.AddChild(startIndex);
 			}
 
-            var currentSuffix = string.Empty;
-            for (int i = text.Length - 1; i >= 0; i--)
-            {
-                var currentChar = text[i];
-                currentSuffix = currentChar + currentSuffix;
-				AddChildToNode(currentSuffix, rootNode);
-            }
+            for (int i = originalText.Length - 1; i >= 0; i--)
+				AddChildToNode(i, rootNode);
         }
 
         private class Node
         {
             private readonly Dictionary<char, Node> children = new Dictionary<char, Node>();
+            private readonly string originalText; // just a pointer, does not take a lot of memory
+            private readonly int startIndex;
 
-            public Node(string text)
+            public Node(int startIndex, int length, string originalText)
             {
-                Text = text;
+                this.originalText = originalText;
+                this.startIndex = startIndex;
+                Length = length;
             }
 
-            public string Text { get; private set; }
+            public int Length { get; private set; }
 
-            public bool FindChild(char c, out Node node)
+            public bool TryGetChild(char c, out Node node)
             {
                 return children.TryGetValue(c, out node);
             }
 
-            public void AddChild(string text)
+            public void AddChild(int startIndex)
             {
-				children.Add(text[0], new Node(text));
+				children.Add(originalText[startIndex], new Node(startIndex, originalText.Length - startIndex, originalText));
             }
 
             public void Split(int index)
             {
-				if (index == 0 || index == Text.Length)
+				if (index == 0 || index == Length)
 					return;
 
-				AddChild(Text.Substring(index));
-                Text = Text.Substring(0, index);
+				AddChild(startIndex + index);
+                Length = index;
+            }
+
+            public int Compare(int originalTextStartIndex)
+            {
+                if (originalTextStartIndex > startIndex)
+                    throw new Exception("Illegal method usage");
+
+                for (int i = startIndex, j = originalTextStartIndex; i - startIndex < Length; i++, j++)
+                    if (originalText[i] != originalText[j])
+                        return i - startIndex;
+
+                return Length;
             }
 
             public override string ToString()
             {
-                return Text + "|" + string.Join('-', children.Select(pair => pair.Value.Text).OrderBy(t => t));
+                return GetText() + "|" + string.Join('-', children.Select(pair => pair.Value.GetText()).OrderBy(t => t));
             }
+
+            private string GetText() => originalText.Substring(startIndex, Length);
         }
     }
 }
